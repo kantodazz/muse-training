bd4117d71f80ac888dd7c74eec6271e747a8334e
repl_muse <repl_muse@muse.go.tz>
2025-09-13 16:59:28 +0300

    Commit Of Bin Changes

---
+++ bin_og/Areas/IFMISTZ/Reports/CommitedContractRPT.aspx.cs
@@ -0,0 +1,41 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IFMIS.Areas.IFMISTZ.Reports
+{
+    public partial class CommitedContractRPT : System.Web.UI.Page
+    {
+        ReportDocument report = new ReportDocument();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var encryptedQueryString = Request.QueryString["rpt"].Replace(" ", "+");
+            var decryptedQueryString = QueryStringModule.Decrypt(encryptedQueryString);
+            var queryStringParameters = decryptedQueryString.Split('&');
+            var queryString = queryStringParameters
+                .Select(queryParam => queryParam.Split('='))
+                .ToDictionary(query => query[0], query => query[1]);
+            {
+                report.Load(Server.MapPath("CommitedContractRPT.rpt"));
+                report.SetParameterValue("@MainInstitutionCode", queryString["inst-code"]);
+                report.SetParameterValue("@FundCategoryId", queryString["fundCategoryId"]);
+                report.SetParameterValue("@CostCenter", queryString["cost-center"]);
+                report.SetParameterValue("@Currency", queryString["currency"]);
+                report.SetParameterValue("@RequiredDate", queryString["req-date"]);
+                //report.SetParameterValue("@EndDate", queryString["end-date"]);
+                CommitedContract.ReportSource = report;
+            }
+            
+        }
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            report.Close();
+            report.Dispose();
+            CommitedContract.Dispose();
+        }
+    }
+}
---
+++ bin_og/Areas/IFMISTZ/Reports/WorkInProgressMovementRPT.aspx.cs
@@ -0,0 +1,37 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace IFMIS.Areas.IFMISTZ.Reports
+{
+    public partial class WorkInProgressMovementRPT : System.Web.UI.Page
+    {
+        ReportDocument report = new ReportDocument();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var encryptedQueryString = Request.QueryString["rpt"].Replace(" ", "+");
+            var decryptedQueryString = QueryStringModule.Decrypt(encryptedQueryString);
+            var queryStringParameters = decryptedQueryString.Split('&');
+            var queryString = queryStringParameters
+                .Select(queryParam => queryParam.Split('='))
+                .ToDictionary(query => query[0], query => query[1]);
+            {
+                report.Load(Server.MapPath("WorkInProgressMovementRPT.rpt"));
+                report.SetParameterValue("@InstitutionCode", queryString["inst-code"]);
+                report.SetParameterValue("@RequiredDate", queryString["required-date"]);
+                WorkInProgressMovement.ReportSource = report;
+            }
+        }
+
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            report.Close();
+            report.Dispose();
+            WorkInProgressMovement.Dispose();
+        }
+    }
+}
