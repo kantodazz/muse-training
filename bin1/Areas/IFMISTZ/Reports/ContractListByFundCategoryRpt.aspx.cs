a688434aab276f065927b713786dfd72ef121ef3
repl_muse <repl_muse@muse.go.tz>
2025-09-13 19:23:49 +0300

    Revert "Commit Deleted Bin1"
    This reverts commit 4f21cfaf51abf1a225d263980dd67f7d739c5920.

---
+++ bin1/Areas/IFMISTZ/Reports/ContractListByFundCategoryRpt.aspx.cs
@@ -0,0 +1,61 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Shared;
+using System.Linq;
+using System;
+using System.Configuration;
+using System.Web.UI;
+
+namespace IFMIS.Areas.IFMISTZ.Reports
+{
+    public partial class ContractListByFundCategoryRpt : Page
+    {
+        ReportDocument report = new ReportDocument();
+
+        TableLogOnInfo _crtableLogoninfo = new TableLogOnInfo();
+        ConnectionInfo _crConnectionInfo = new ConnectionInfo();
+        Tables _crTables;
+	
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string[] strConnection = ConfigurationManager.ConnectionStrings["IFMISTZReportsContext"].ConnectionString.Split(new char[] { ';' });
+
+            var encryptedQueryString = Request.QueryString["rpt"].Replace(" ", "+");
+            var decryptedQueryString = QueryStringModule.Decrypt(encryptedQueryString);
+            var queryStringParameters = decryptedQueryString.Split('&');
+            var queryString = queryStringParameters
+                .Select(queryParam => queryParam.Split('='))
+                .ToDictionary(query => query[0], query => query[1]);
+
+            //if (User.Identity.IsAuthenticated)
+            {
+                report.Load(Server.MapPath("ContractListByFundCategoryRpt.rpt"));
+                report.SetParameterValue("@InstitutionCode", queryString["inst-code"]);
+                report.SetParameterValue("@StartDate", queryString["start-date"]);
+                report.SetParameterValue("@EndDate", queryString["end-date"]);
+                report.SetParameterValue("@CostCenter", queryString["cost-center"]);
+                report.SetParameterValue("@FundCategoryId", queryString["fundCategoryId"]);
+                ContractListByFundCategoryRPT.ReportSource = report;
+            }
+
+            _crConnectionInfo.ServerName = strConnection[0].Split(new char[] { '=' }).GetValue(1).ToString();
+            _crConnectionInfo.DatabaseName = strConnection[1].Split(new char[] { '=' }).GetValue(1).ToString();
+            _crConnectionInfo.UserID = strConnection[3].Split(new char[] { '=' }).GetValue(1).ToString();
+            _crConnectionInfo.Password = strConnection[4].Split(new char[] { '=' }).GetValue(1).ToString();
+
+            _crTables = report.Database.Tables;
+			
+            foreach (Table crTable in _crTables)
+            {
+                _crtableLogoninfo = crTable.LogOnInfo;
+                _crtableLogoninfo.ConnectionInfo = _crConnectionInfo;
+                crTable.ApplyLogOnInfo(_crtableLogoninfo);
+            }
+        }
+        protected void Page_Unload(object sender, EventArgs e)
+        {
+            report.Close();
+            report.Dispose();
+            ContractListByFundCategoryRPT.Dispose();
+        }
+    }
+}
